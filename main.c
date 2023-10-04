13ed62c46f77fb7b39c7ce53e9ae5c08b7ca9179
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-04 21:28:57 +0100

    README.md created, isalnum tested and working

--- main.c
+++ main.c
@@ -8,12 +8,14 @@ int main(void)
    char c = '0';
 
    /* Tests for ft_isalpha */
-   printf("isalpha(%c) => %i\n", c, isalpha(c)); 
-   printf("ft_isalpha(%c) => %i\n", c, ft_isalpha(c));
+   // printf("isalpha(%c) => %i\n", c, isalpha(c)); 
+   // printf("ft_isalpha(%c) => %i\n", c, ft_isalpha(c));
 
    /* Tests for ft_isdigit */
    // printf("isdigit(%c) => %i\n", c, isdigit(c));
    // printf("ft_isdigit(%c) => %i\n", c, ft_isdigit(c));
 
    /* Tests for ft_isalnum */
+   printf("isalnum(%c) => %i\n", c, isalnum(c));
+   printf("ft_isalnum(%c) => %i\n", c, ft_isalnum(c));
 }
