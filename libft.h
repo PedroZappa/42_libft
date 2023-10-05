970bceab4327d5de9b67bef6724cfe61b47e52ab
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-05 19:27:25 +0100

    libft.h : added standard libraries and memset()

--- libft.h
+++ libft.h
@@ -6,14 +6,17 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:48:04 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/05 12:01:06 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/05 19:25:52 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -21,5 +24,6 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif
