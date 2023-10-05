a2ce53d368ea385b77032f5136f4fceb69e1b739
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-05 12:02:22 +0100

    ft_strlen.c : Done and Tested

---
+++ ft_strlen.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 11:33:20 by zedr0             #+#    #+#             */
+/*   Updated: 2023/10/05 11:59:56 by zedr0            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+/*
+#include <stdio.h>	// printf()
+#include <string.h> // strlen()
+
+int main(int argc, char *argv[])
+{
+	char *str = "Zedr0";
+	
+	if (argc == 2)
+		str = argv[argc-1];
+
+	// Tests for ft_isalnum
+	printf("strlen(%s) => %lu\n", str, strlen(str));
+	printf("ft_strlen(%s) => %lu\n", str, ft_strlen(str));
+}
+*/
