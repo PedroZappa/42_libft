30977a9f43235f1844381ed3f88d5216bd953945
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-05 10:57:35 +0100

    ft_isalpha : Tested and working

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,14 +6,30 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 10:12:08 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/03 11:18:15 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/05 10:48:57 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) 
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (c);
 	else
 		return (0);
 }
+/*
+#include <ctype.h>  // isalpha()
+#include <stdio.h>  // printf()
+
+int main(int argc, char *argv[])
+{
+	char c = '*';
+	
+	if (argc == 2)
+		c = argv[argc-1][0];
+
+	// Tests for ft_isalnum
+	printf("isalpha(%c) => %i\n", c, isalpha(c));
+	printf("ft_isalpha(%c) => %i\n", c, ft_isalpha(c));
+}
+*/
