498d9c88168d993cf1713e0c5a6fd80eb16180ed
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-05 12:01:48 +0100

    libft.h : norm checked; ft_strlen.c added

--- libft.h
+++ libft.h
@@ -6,17 +6,20 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:48:04 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/05 11:27:28 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/05 12:01:06 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
