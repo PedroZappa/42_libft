68fdbf168972894bc49670c21c382e45041e3009
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-05 11:28:29 +0100

    libft.h : norm checked; ft_isprint.c added

--- libft.h
+++ libft.h
@@ -6,17 +6,17 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:48:04 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/05 10:28:38 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/05 11:27:28 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#	define LIBFT_H
+# define LIBFT_H
 
 int	ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
 
 #endif
-
