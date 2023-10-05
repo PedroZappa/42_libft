7afac7633e154d0a7be4680ad77370fea57bb0cb
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-05 11:05:17 +0100

    ft_isalnum.c : Done & tested

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 12:24:17 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/03 15:56:03 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/05 11:04:42 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,22 @@ int	ft_isalnum(int c)
 {
 	if (ft_isalpha(c) || ft_isdigit(c))
 		return (c);
-	else 
+	else
 		return (0);
 }
+/*
+#include <ctype.h>  // isalnum()
+#include <stdio.h>  // printf()
+
+int main(int argc, char *argv[])
+{
+	char c = '*';
+	
+	if (argc == 2)
+		c = argv[argc-1][0];
+
+	// Tests for ft_isalnum
+	printf("isalnum(%c) => %i\n", c, isalnum(c));
+	printf("ft_isalnum(%c) => %i\n", c, ft_isalnum(c));
+}
+*/
