3635961e01fe126aff79a2bfef6db37a94030c28
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-05 20:12:02 +0100

    ft_bzero : Implemented, tested, added to Makefile & libft

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,47 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 19:31:02 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/05 19:42:01 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/05 20:07:30 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-	ft_memset(s, 0, n);
+	char	*ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = (char *) s;
+	while (i < n)
+		ptr[i++] = '\0';
 }
+/*
+int main(int argc, char *argv[])
+{
+	char *str;
+	size_t n = 3;
+	
+	if (argc == 2)
+		str = argv[argc-1];
+	else if (argc == 3)
+	{
+		str = argv[argc-2];
+		n = atoi(argv[argc-1]);
+	}
+	else
+	{
+		printf("Usage: %s [c] [n]\n", argv[0]);
+		return (0);
+	}
+
+	// Tests
+	printf("argc => %d\n", argc);
+	printf("str pre-bzero() => '%s'	| n => %lu\n", str, n);
+	ft_bzero(str, n);
+	printf("str post-bzero() => '%s'\n", str);
+	
+	return (0);
+}
+*/
