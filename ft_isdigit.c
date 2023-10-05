78f8fc45a6d46bcb100b22712206581fceacbec6
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-05 11:00:53 +0100

    ft_isdigit.c : Implemented w/ test

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 11:51:59 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/03 12:08:46 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/05 11:00:08 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,19 @@ int	ft_isdigit(int d)
 	else
 		return (0);
 }
+/*
+#include <ctype.h>  // isdigit()
+#include <stdio.h>  // printf()
+
+int main(int argc, char *argv[])
+{
+	char c = '*';
+	
+	if (argc == 2)
+		c = argv[argc-1][0];
+
+	// Tests for ft_isalnum
+	printf("isdigit(%c) => %i\n", c, isdigit(c));
+	printf("ft_isdigit(%c) => %i\n", c, ft_isdigit(c));
+}
+*/
