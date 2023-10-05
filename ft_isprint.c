588163c81d73b4bea35330e95062adcc1c19dd84
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-05 11:29:24 +0100

    ft_isprint.c : Done & tested

---
+++ ft_isprint.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 11:18:44 by zedr0             #+#    #+#             */
+/*   Updated: 2023/10/05 11:24:00 by zedr0            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isprint(int c)
+{
+	return (c >= 32 && c <= 126);
+}
+/*
+#include <ctype.h>  // isprint()
+#include <stdio.h>  // printf()
+
+int main(int argc, char *argv[])
+{
+	char c = '*';
+	
+	if (argc == 2)
+		c = argv[argc-1][0];
+
+	// Tests for ft_isalnum
+	printf("isprint(%c) => %i\n", c, isprint(c));
+	printf("ft_isprint(%c) => %i\n", c, ft_isprint(c));
+}
+*/
