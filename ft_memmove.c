c93f8d0d3996fed5a492ffa138c75b9b204394d2
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-06 11:18:47 +0100

    ft_memove.c : Done and tested

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 09:42:59 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/06 10:17:22 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/06 11:16:59 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,3 +33,31 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
+/*
+int main(int argc, char *argv[])
+{
+	char *dest = malloc(42);
+	char *src = NULL;
+	size_t n = 3;
+	
+	if (argc == 2)
+		src = argv[argc-1];
+	else if (argc == 3)
+	{
+		src = argv[argc-2];
+		n = atoi(argv[argc-1]);
+	}
+	else
+	{
+		printf("Usage: %s [src] [n]\n", argv[0]);
+		return (0);
+	}
+
+	// Tests
+	printf("src => '%s'	| n => %ld\n", src, n);
+	ft_memmove(dest, src, n);
+	printf("dest => '%s'\n", dest);
+
+	return (0);
+}
+*/
