b13ade050b5349082d1c004ca9a62418872022d7
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-07 20:39:31 +0100

    ft_strchr & ft_strrchr : Tests Implemented

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 21:11:48 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/06 21:17:08 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/07 20:32:29 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,17 @@ char	*ft_strchr(const char *s, int c)
 {
 	return (ft_memchr(s, c, ft_strlen(s) + 1));
 }
+/*
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Usage: %s <string> <char>\n", argv[0]);
+		return (0);
+	}
+
+	// Tests
+	printf("ft_strchr => %s\n", ft_strchr(argv[1], argv[2][0]));
+	return (0);
+}
+*/
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 10:14:39 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/07 10:21:20 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/07 20:38:43 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,17 @@ char	*ft_strrchr(const char *s, int c)
 	}
 	return (NULL);
 }
+/*
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Usage: %s <string> <char>\n", argv[0]);
+		return (0);
+	}
+
+	// Tests
+	printf("ft_strrchr => %s\n", ft_strrchr(argv[1], argv[2][0]));
+	return (0);
+}
+*/
