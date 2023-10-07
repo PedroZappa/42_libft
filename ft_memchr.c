7991646011ec3d98374c25b9865940e98b4beb98
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-07 21:27:00 +0100

    ft_memchr : test

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 21:00:08 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/06 21:10:09 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/07 21:03:39 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,20 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
+/*
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Usage: %s <string> <char>\n", argv[0]);
+		return (0);
+	}
+
+	// Tests
+	printf("Input string:\t%s\n", argv[1]);	
+	printf("Input address:\t%p\n", argv[1]);	
+	printf("ft_memchr found '%c' at address:\n", argv[2][0]);
+	printf("\t\t%p\n", ft_memchr(argv[1], argv[2][0], ft_strlen(argv[1])));
+	return (0);
+}
+*/
