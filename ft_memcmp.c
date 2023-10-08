1d0caaaed6bc8919ad23ed3bd8bff8e7b27dbf02
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-08 10:57:39 +0100

    ft_memcmp : tests added

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 10:30:59 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/08 10:44:23 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/08 10:55:36 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,17 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
+/*
+int main (int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		printf("Usage: %s str1 str2 n\n", argv[0]);
+		return (0);
+	}
+	printf("ft_memcmp( %s, %s, %d ) = %d\n", argv[1], argv[2], atoi(argv[3]), 
+		ft_memcmp(argv[1], argv[2], atoi(argv[3])));
+	return (0);
+
+}
+*/
