2f16ab2068a46ee45b2aa4a925fdde882803a46e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-08 18:53:54 +0100

    ft_strnstr: Done, passing francinette

---
+++ ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/08 17:35:30 by zedr0             #+#    #+#             */
+/*   Updated: 2023/10/08 18:52:00 by zedr0            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!big && len == 0)
+		return (NULL);
+	if (!little[0])
+		return ((char *)big);
+	i = 0;
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] && big[i + j] == little[j] && (i + j) < len)
+		{
+			if (!little[j + 1])
+				return ((char *)(big + i));
+			++j;
+		}
+		++i;
+	}
+	return (NULL);
+}
