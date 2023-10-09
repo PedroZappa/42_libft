c87464266932fe8c229cafee399c7f3bb06737e3
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-09 19:30:48 +0100

    ft_strdup.c : implemented, not tested

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:32:54 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/09 13:03:05 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/09 19:28:17 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 char	*strdup(const char *s)
 {
-	
+	size_t len;
+	char *dst;
+
+	len = ft_strlen(s);
+	dst = malloc(len + 1);
+	if (!dst)
+		return (NULL);
+	ft_memcpy(dst, s, len);
+	return (dst);
 }
