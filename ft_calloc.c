1950838410dcd4dab7d95f0bde4b5166a71a898f
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-09 11:49:00 +0100

    ft_calloc : Working!

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 09:57:51 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/09 10:25:57 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/09 11:46:01 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,13 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	char	*ptr;
+	void	*ptr;
 	size_t	to_alloc;
 
 	to_alloc = (nmemb * size);
-	ptr = (char *)malloc(sizeof(char) * to_alloc);
+	ptr = malloc(sizeof(char) * to_alloc);
 	if (!ptr)
 		return (NULL);
-	while (--size >= 0)
-		ptr[size] = '\0';
-	return ((void *)ptr);
+	ft_bzero(ptr, to_alloc);
+	return (ptr);
 }
