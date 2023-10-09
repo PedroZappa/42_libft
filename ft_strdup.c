8e7096e6a4198e9c2850fb875591a032f0574d8f
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-09 19:55:00 +0100

    ft_strdup.c : passes francinette, needs tests

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,21 +6,26 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:32:54 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/09 19:28:17 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/09 19:54:07 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-	size_t len;
-	char *dst;
+	char	*dst;
+	size_t	i;
 
-	len = ft_strlen(s);
-	dst = malloc(len + 1);
+	dst = malloc(ft_strlen(s) * sizeof(char) + 1);
 	if (!dst)
 		return (NULL);
-	ft_memcpy(dst, s, len);
+	i = 0;
+	while (s[i])
+	{
+		dst[i] = s[i];
+		++i;
+	}
+	dst[i] = '\0';
 	return (dst);
 }
