2d454ee161ee0b19e243e3d6407215b8d5397da7
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-09 12:31:22 +0100

    .ft_calloc : Test implemented

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 09:57:51 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/09 11:46:01 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/09 12:17:50 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,21 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ft_bzero(ptr, to_alloc);
 	return (ptr);
 }
+/*
+int main (int argc, char *argv[])
+{
+	void *calloced;
+	if (argc != 3)
+	{
+		printf("Usage: %s <nmemb> <size>", argv[0]);
+		return (0);
+	}
+	// Tests
+	printf("ft_calloc( %i, %i )\n", 
+			ft_atoi(argv[1]), ft_atoi(argv[2]));
+	calloced = ft_calloc(ft_atoi(argv[1]), ft_atoi(argv[2]));
+	printf("Start of ft_calloc() :\t%p\n", calloced);
+	printf("Last allocated mem loc:\t%p\n",
+			(calloced + ft_atoi(argv[2])));
+}
+*/
