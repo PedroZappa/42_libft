59486fb9a014de4b1228f486d2e59e8557a77a1a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-10 11:21:49 +0100

    ft_putchar_fd : Zedro's test implemented

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 10:01:10 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/10 10:01:32 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/10 11:21:08 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,19 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+/*
+int main (int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Usage: %s <char> <fd>\n", argv[0]);
+		return (0);
+	}
+
+	// Tests
+	printf("");
+	ft_putchar_fd(argv[1][0], ft_atoi(argv[2]));
+	ft_putchar_fd('\n', ft_atoi(argv[2]));
+	return (0);
+}
+*/
