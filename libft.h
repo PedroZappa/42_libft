85f427c9df587ec68a2d6c995af37709d6b84f7a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-10 18:23:45 +0100

    ft_strtrim : Implemented, passes francinette

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:18:48 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/10 17:47:38 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/10 18:03:27 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ char	*ft_strdup(const char *s);
 // Mandatory Part 2
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
