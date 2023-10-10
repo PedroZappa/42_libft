6a0f47e2baeb8d0c288630be4b63e3027f9f1ba3
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-10 11:16:24 +0100

    ft_putnbr_fd : Implemented, passes francinette, needs Zedro's tests

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 10:14:56 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/10 10:20:24 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/10 11:11:01 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 	{
-		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		ft_putnbr_fd(147483648, fd);
+		ft_putstr_fd("-2147483648", fd);
+		return ;
 	}
 	else if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
 		n = -n;
 		ft_putnbr_fd(n, fd);
+		return ;
 	}
 	else if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
 		ft_putnbr_fd(n % 10, fd);
 	}
-	else if (ft_isdigit(n))
+	else 
 		ft_putchar_fd(n + '0', fd);
 }
