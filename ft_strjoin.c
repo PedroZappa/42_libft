bb6ed78406b5963f81009e026530367e3f73bf3f
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-10 18:00:23 +0100

    ft_strjoin : Re-Implemented, shorter, passes francinette

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 17:39:33 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/10 17:45:38 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/10 17:58:56 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	size_t	i;
-	size_t	j;
 	size_t	s1_len;
 	size_t	s2_len;
 
@@ -27,13 +25,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!str)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (s1[i])
-		str[i++] = s1[j++];
-	j = 0;
-	while (s2[j])
-		str[i++] = s2[j++];
-	str[i] = '\0';
+	ft_memcpy(str, s1, s1_len);
+	ft_memcpy(str + s1_len, s2, s2_len);
+	str[s1_len + s2_len] = '\0';
 	return (str);
 }
