87d7076f5eb07a19bee17ba8adb85bfba0d10478
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-10 11:32:59 +0100

    ft_putchar_fd : Zedro's test improved

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 10:01:10 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/10 11:21:08 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/10 11:32:13 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,24 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-/*
-int main (int argc, char **argv)
+
+int main (int argc, char *argv[])
 {
+	char c;
+	int fd;
+
 	if (argc != 3)
 	{
 		printf("Usage: %s <char> <fd>\n", argv[0]);
 		return (0);
 	}
-
+	c = argv[1][0];
+	fd = ft_atoi(argv[2]);
 	// Tests
-	printf("");
-	ft_putchar_fd(argv[1][0], ft_atoi(argv[2]));
-	ft_putchar_fd('\n', ft_atoi(argv[2]));
+	printf("ft_putchar_fd('%c', %d) => ", c, fd);
+	fflush(stdout);
+	ft_putchar_fd(c, fd);
+	ft_putchar_fd('\n', fd);
 	return (0);
 }
-*/
+
