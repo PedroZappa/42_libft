a921ec2a80166ac0719987a628a8e7860dea5ae0
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-10 11:43:11 +0100

    ft_putendl_fd : Zedro's test implemented

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 10:11:11 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/10 10:11:28 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/10 11:42:37 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,23 @@ void	ft_putendl_fd(char *s, int fd)
 		ft_putchar_fd(*s++, fd);
 	ft_putchar_fd('\n', fd);
 }
+
+int main (int argc, char **argv)
+{
+	char *s;
+	int fd;
+
+	if (argc != 3)
+	{
+		printf("Usage: %s <str> <fd>\n", argv[0]);
+		return (0);
+	}
+	s = argv[1];
+	fd = ft_atoi(argv[2]);
+	// Tests
+	printf("ft_putendl_fd('%s', %d) => ", s, fd);
+	fflush(stdout);
+	ft_putendl_fd(s, fd);
+	return (0);
+}
+
