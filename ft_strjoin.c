93cb51f78d6739126371036539a2115d6923e4ed
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-11 11:49:45 +0100

    ft_strjoin : Zedro's test

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 17:39:33 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/10 20:00:20 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/11 11:48:47 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,3 +30,26 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str[s1_len + s2_len] = '\0';
 	return (str);
 }
+/*
+int main(int argc, char *argv[])
+{
+	char	*str1;
+	char	*str2;
+	char	*joined;
+
+	if (argc != 3)
+	{
+		printf("Usage: %s str1 str2", argv[0]);
+		return (1);
+	}
+	str1 = argv[1];
+	str2 = argv[2];
+	joined = ft_strjoin(str1, str2);
+	// Tests
+	printf("ft_strjoin( '%s', '%s') = ", str1, str2);
+	printf("'%s'\n", joined);
+
+	free(joined);
+	return (0);
+}
+*/
