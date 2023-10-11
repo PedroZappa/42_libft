26dc00d4c4a48da0d63fb67bfdaf97d4547a48a9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-11 14:42:07 +0100

    ft_strmapi : Test half-implemented

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 20:53:48 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/10 20:55:44 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/11 14:39:09 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,31 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	}
 	str[i] = '\0';
 	return (str);
+}
+
+char conv(unsigned int n, char c)
+{
+	if ((n % 2) == 0)
+		c = ft_toupper(c);
+	return (c);
+}
+
+int main(int argc, char *argv[])
+{
+	char *str;
+	char (*func)(unsigned int, char);
+	char c;
+	int i;
+
+	if (argc != 3)
+	{
+		printf("Usage: %s <string>", argv[0]);
+		return (1);
+	}
+	str = argv[1];
+	func = argv[2];
+	// Tests
+	printf("strmapi(%s, %s) = ", str, func);
+	printf("%s\n", ft_strmapi(str, func));
+	return (0);
 }
