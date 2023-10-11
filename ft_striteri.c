ee78a899dd605024f2ea42f564b227b734b27b2e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-11 21:18:55 +0100

    ft_striteri : Implemented, test not wroking yet

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 20:59:59 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/10 21:16:59 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/11 21:18:01 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,34 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 		f(i, (s + i));
 		++i;
 	}
+}
+
+void	ft_change(unsigned int i, char *str)
+{
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	char *str;
+
+	if (argc != 2)
+	{
+		printf("Usage: %s <string>", argv[0]);
+		return (1);
+	}
+	str = argv[1];
+
+	// Tests
+	printf("striteri(%s, change) = ", str);
+	ft_striteri(str, ft_change);
+	printf("%s\n", str);
+
+	return (0);
 }
