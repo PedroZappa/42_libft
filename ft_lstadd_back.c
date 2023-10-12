88527bb576f339ac9f94d3078d2c92212aaa83d8
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-12 12:48:30 +0100

    ft_lstadd_back : Implemented. passes Francinette

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,21 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/12 10:21:46 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/12 12:26:54 by passunca         ###   ########.fr       */
+/*   Created: 2023/10/12 12:00:06 by passunca          #+#    #+#             */
+/*   Updated: 2023/10/12 12:47:10 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	if (!new || !lst)
+	t_list	*tmp;
+
+	if (!*lst)
+	{
+		*lst = new;
 		return ;
-	new->next = *lst;
-	*lst = new;
+	}
+	tmp = *lst;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:18:48 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/12 11:55:26 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/12 12:43:04 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
