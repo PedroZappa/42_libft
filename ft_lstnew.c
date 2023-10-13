9a0f76260e690527189a38b2b7a2224027d97d4f
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-13 08:46:48 +0100

    ft_lstnew : Zedro's test implemented

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 09:47:32 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/12 10:19:34 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/13 08:45:08 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,25 @@ t_list	*ft_lstnew(void *content)
 int main(int argc, char *argv[])
 {
 	t_list	*node;
+	char	*content;
+
+	if (argc != 2)
+	{
+		printf("Usage: %s <string>\n", argv[0]);
+		return (0);
+	}
+	content = argv[1];
+	
+	node = ft_lstnew(content);
+	printf("ft_lstnew(\"%s\") returns:\n", content);
+	printf("node:\n");
+	printf("\tAddress: %p\n", node);
+
+	printf("node->content:\n");
+	printf("\tAddress: %p\n", node->content);
+	printf("\tValue :\t\"%s\"\n", (char *) node->content);
+
+	printf("node->next:\n");
+	printf("\tAddress: %p\n", node->next);
 }
 */
