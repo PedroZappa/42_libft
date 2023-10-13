f8761f8d7a77c96a29f61e67525626fb7aa517fc
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-13 10:35:01 +0100

    ft_lstadd_back : Zedro's test, added print of node address

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 12:00:06 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/12 12:47:10 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/13 10:33:26 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,53 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*tmp;
+	t_list	*ptr;
 
 	if (!*lst)
 	{
 		*lst = new;
 		return ;
 	}
-	tmp = *lst;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
+	ptr = *lst;
+	while (ptr->next)
+		ptr = ptr->next;
+	ptr->next = new;
+}
+
+int main(int argc, char *argv[])
+{
+	t_list	*node;
+	t_list	*back_node;
+	char	*content1;
+	char	*content2;
+	size_t	node_n;
+
+	if (argc != 3)
+	{
+		printf("Usage: %s <content1> <content2>\n", argv[0]);
+		return (1);
+	}
+	content1 = argv[1];
+	content2 = argv[2];
+
+	node = ft_lstnew(content1);
+	if (!node)
+		return (1);
+	back_node = ft_lstnew(content2);
+	if (!back_node)
+		return (1);
+	ft_lstadd_back(&node, back_node);
+	
+	// Print List
+	printf("Printing List :\n");
+	node_n = 1;
+	while (node)
+	{
+		printf("node %zu:\t\t\"%p\"\n", node_n, node);
+		printf("\t->content: \"%s\"\n", (char *) node->content);
+		printf("\t->next: \"%p\"\n", (char *) node->next);
+		node = node->next;
+		++node_n;
+	}
+	return (0);
 }
