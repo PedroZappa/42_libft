77e338644116fbba938eabf1b6b39843552921fb
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-13 10:48:28 +0100

    ft_lstsize : Zedro's test

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: passunca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:15:18 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/12 12:47:41 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/13 10:46:59 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,31 @@ int	ft_lstsize(t_list *lst)
 		++count;
 	}
 	return (count);
+}
+
+int main(int argc, char *argv[])
+{
+	t_list	*node;
+	size_t	node_n;
+	char	*content;
+	size_t	lst_size;
+	
+	if (argc != 3)
+	{
+		printf("Usage: %s <content> <lst_size>\n", argv[0]);
+		return (1);
+	}
+	content = argv[1];
+	lst_size = ft_atoi(argv[2]);
+
+	// Create List
+	node = ft_lstnew(content);
+	if (!node)
+		return (1);
+	while (--lst_size)
+		ft_lstadd_back(&node, ft_lstnew(content));
+
+	// Count List Size
+	node_n = ft_lstsize(node);
+	printf("List Size: %zu\n", node_n);
 }
