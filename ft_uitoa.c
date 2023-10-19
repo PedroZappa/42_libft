6d5d1933fef92af1889f8943ca079f0b5773a52e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-19 20:12:17 +0100

    ft_uito.c : Created, implemented, and added to libft and Makefile

---
+++ ft_uitoa.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_uitoa.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 19:58:55 by zedr0             #+#    #+#             */
+/*   Updated: 2023/10/19 20:10:40 by zedr0            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	*ft_alloc_n(char* str, int len, unsigned int n, unsigned int n_bak);
+
+char	*ft_uitoa(unsigned int n)
+{
+	char			*str;
+	int				len;
+	unsigned int	n_bak;
+
+	n_bak = n;
+	len = ft_numlen(n, 10);
+	str = NULL;
+	str = ft_alloc_n(str, len, n, n_bak);
+	if (str == NULL)
+		return (NULL);
+	return (str);
+}
+
+static char	*ft_alloc_n(char* str, int len, unsigned int n, unsigned int n_bak)
+{
+	if (n_bak == 0)
+		len = 1;
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+	while (len-- > 0)
+	{
+		str[len] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
+}
