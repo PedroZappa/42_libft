999df459af5e43ba14e63a8510adff7f7cb2954a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-30 14:52:08 +0000

    ft_putnbr : implemented

---
+++ ft_putnbr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 14:41:37 by passunca          #+#    #+#             */
+/*   Updated: 2023/10/30 14:51:51 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_putnbr(int n)
+{
+	int len;
+
+	len = ft_numlen(n, 10);
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", 1);
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', 1);
+		ft_putnbr(-n);
+	}
+	else if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putchar_fd(n % 10 + '0', 1);
+	}
+	else
+		ft_putchar_fd(n + '0', 1);
+	return (len);
+}
+//
+// int main(int argc, char *argv[])
+// {
+// 	if (argc != 2)
+// 	{
+// 		printf("Usage: %s <number>\n", argv[0]);
+// 		return (1);
+// 	}
+// 	ft_putnbr(ft_atoi(argv[1]));
+// 	printf("\n");
+// 	return (0);
+// }
