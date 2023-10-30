a0891f4852d6b68601a71a5c997e1426db1229a4
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-30 11:24:47 +0000

    libft : ft_uputnbr_fd removed fd arg

--- ft_uputnbr_fd.c
+++ ft_uputnbr_fd.c
@@ -6,21 +6,21 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 09:53:54 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/30 11:23:09 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/30 11:24:26 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_uputnbr_fd(unsigned int n, int fd)
+void	ft_uputnbr_fd(unsigned int n)
 {
 	if (n > 10)
 	{
-		ft_uputnbr_fd(n / 10, 1);
-		ft_uputnbr_fd(n % 10, 1);
+		ft_uputnbr_fd(n / 10);
+		ft_uputnbr_fd(n % 10);
 	}
 	else
-		ft_putchar_fd((n + '0'), 1);
+		ft_putchar_fd((n + '0'));
 
 }
 //
