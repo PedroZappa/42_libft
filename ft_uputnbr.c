caf786149f3feb22c365fe8576035dbae6889c51
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-30 12:26:42 +0000

    libft : putchar_fd updated

--- ft_uputnbr.c
+++ ft_uputnbr.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 09:53:54 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/30 12:18:47 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:25:43 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	ft_uputnbr(unsigned int n)
 		ft_uputnbr(n % 10);
 	}
 	else
-		ft_putchar(n + '0');
+		ft_putchar_fd((n + '0'), 1);
 }
 
 int	main(int argc, char *argv[])
