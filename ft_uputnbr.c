339cfc2fda1708f75cdd7fc6d6c7ddf0fc40f09a
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-30 12:51:06 +0000

    libft : ft_uputnbr updated

--- ft_uputnbr.c
+++ ft_uputnbr.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 09:53:54 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/30 12:33:57 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:50:49 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ void	ft_uputnbr(unsigned int n)
 		ft_putstr_fd("4294967295", 1);
 		return ;
 	}
-	if (n < 0)
+	else if (n < 0)
 	{
 		n = -n;
 		ft_uputnbr(n);
 	}
-	if (n >= 10)
+	else if (n >= 10)
 	{
 		ft_uputnbr(n / 10);
 		ft_uputnbr(n % 10);
