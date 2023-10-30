9ceb7a8b30216b82e9ec1ec485c355baed326c7e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-30 12:08:37 +0000

    libft : commented test code

--- ft_uputnbr.c
+++ ft_uputnbr.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 09:53:54 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/30 12:02:33 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:08:06 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,20 @@ void	ft_uputnbr(unsigned int n)
 	else
 		ft_putchar(n + '0');
 }
-
-int	main(int argc, char *argv[])
-{
-	unsigned int	n;
-	int				*count;
-
-	count = NULL;
-	if (argc != 2)
-	{
-		printf("Usage: %s <number>\n", argv[0]);
-		return (1);
-	}
-	n = ft_atoi(argv[1]);
-	ft_uputnbr(n);
-	printf("\n");
-	return (0);
-}
+//
+// int	main(int argc, char *argv[])
+// {
+// 	unsigned int	n;
+// 	int				*count;
+//
+// 	count = NULL;
+// 	if (argc != 2)
+// 	{
+// 		printf("Usage: %s <number>\n", argv[0]);
+// 		return (1);
+// 	}
+// 	n = ft_atoi(argv[1]);
+// 	ft_uputnbr(n);
+// 	printf("\n");
+// 	return (0);
+// }
