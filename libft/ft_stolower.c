621585190ce70fabd3c206a9377e3cb5d387d20c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-04 11:22:14 +0000

    ft_stolower.c : Tests and fixed

--- libft/ft_stolower.c
+++ libft/ft_stolower.c
@@ -6,22 +6,29 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 11:02:32 by passunca          #+#    #+#             */
-/*   Updated: 2024/02/04 11:06:49 by passunca         ###   ########.fr       */
+/*   Updated: 2024/02/04 11:21:51 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
 /*	Converts a string to lowercase.
  *	*/
 char	*ft_stolower(char *str)
 {
-	while (*str)
-	{
-		if (ft_isupper(*str))
-			*str += 32;
-		++str;
-	}
+	int		i;
+
+	i = -1;
+	while (str[++i])
+		if (ft_isupper(str[i]))
+			str[i] = ft_tolower(str[i]);
 	return (str);
+}
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[1]);
+	printf("%s\n", ft_stolower(argv[1]));
+	return 0;
 }
