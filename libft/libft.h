cf56e86dc67ba25328cdce6f7de3d4095710585b
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-02-11 21:59:26 +0000

    libft.h : guarded printf.h

--- libft/libft.h
+++ libft/libft.h
@@ -18,7 +18,10 @@
 # include <unistd.h>
 # include <stddef.h>
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 # include "../ft_printf/ft_printf.h"
+#endif
 # include "../get_next_line/get_next_line.h"
 # include "../color_codes/ansi.h"
 # include "../color_codes/hexa.h"
