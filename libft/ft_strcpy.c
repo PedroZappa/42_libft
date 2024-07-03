eb8c3892c846e94e2186abc16e740d56d0046b5f
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-07-03 15:24:57 +0100

    libft/ft_strcpy.c : debug

--- libft/ft_strcpy.c
+++ libft/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:13:31 by passunca          #+#    #+#             */
-/*   Updated: 2024/07/03 15:13:58 by passunca         ###   ########.fr       */
+/*   Updated: 2024/07/03 15:24:51 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strcpy(char *dest, const char *src)
 	size_t	i;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (src[i+1] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
