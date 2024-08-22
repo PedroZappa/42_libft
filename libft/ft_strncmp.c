667508e1163cd17b6f834e50221a74346397cf51
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-08-22 11:09:48 +0100

    libft/ft_strncmp.c : gblm fix

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -28,7 +28,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] != '\0') && (i < n))
 	{
 		if ((s2[i] != '\0') && (s1[i] != s2[i]))
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+			return (s1[i] - s2[i]);
 		++i;
 	}
 	if ((s2[i] != '\0') && (i != n))
