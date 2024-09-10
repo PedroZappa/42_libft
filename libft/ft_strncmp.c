a3f62dc9c0ecf3ef44226c43b3bb73ccaedfb844
Gabriel Fragoso Lima <gfragoso@student.42porto.com>
2024-09-10 15:40:30 +0100

    fix strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -27,7 +27,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while ((s1[i] != '\0') && (i < n))
 	{
-		if ((s2[i] != '\0') && (s1[i] != s2[i]))
+		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		++i;
 	}
